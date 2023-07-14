91d3d436aaa7c39350b8d48b08ad3930c8426272
Vuyisile Ndlovu <terrameijar@gmail.com>
2023-07-14 10:24:42 +0200

    Update Django to v4

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -2,12 +2,12 @@ import os
 
 import dotenv
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
